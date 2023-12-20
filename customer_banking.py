42725d286d5e1001e987bc636f28bd52c7fad934
Codesseus <cody@ccushing.com>
2023-12-19 19:40:04 -0700

    Push that has all the functionality of calculating and pritning the balance and interests

--- customer_banking.py
+++ customer_banking.py
@@ -1,5 +1,31 @@
 # Import the create_cd_account and create_savings_account functions
 # ADD YOUR CODE HERE
+from savings_account import create_savings_account
+from cd_account import create_cd_account
+
+def check_user_input_float(user_input):
+    """
+    function that validates the users input is actually a float, 
+    if valid returns the float and False
+    otherwise it returns None and True
+    """
+    if user_input.isdigit():
+        return float(user_input), True
+
+    print("The inputed value was not a number, you must input a number")
+    return None, False
+
+def check_user_input_int(user_input):
+    """
+    function that validates the users input is actually an int, 
+    if valid returns the float and False
+    otherwise it returns None and True
+    """
+    if user_input.isdigit():
+        return int(user_input), True
+
+    print("The inputed value was not a number, you must input a number")
+    return None, False
 
 # Define the main function
 def main():
@@ -10,20 +36,51 @@ def main():
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     # ADD YOUR CODE HERE
 
+    savings_balance_is_digit = False
+    savings_interest_is_digit = False
+    savings_maturity_is_digit = False
+
+    while not savings_balance_is_digit:
+        savings_balance, savings_balance_is_digit = check_user_input_float(input("What is the starting balance for the savings account? "))
+
+    while not savings_interest_is_digit:
+        savings_interest, savings_interest_is_digit = check_user_input_float(input("What is the interest rate for the saving account? "))
+
+    while not savings_maturity_is_digit:
+        savings_maturity, savings_maturity_is_digit = check_user_input_int(input("What is the maturity(number of months) of the savings account? "))
+
+
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    print(f"Updated saving balance: ${updated_savings_balance:,.2f}")
+    print(f"Interest earned: ${interest_earned:,.2f}")
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
+    cd_balance_is_digit = False
+    cd_interst_is_digit = False
+    cd_maturity_is_digit = False
+
+    while not cd_balance_is_digit:
+        cd_balance, cd_balance_is_digit = check_user_input_float(input("What is the starting balance for the cd account? "))
+
+    while not cd_interst_is_digit:
+        cd_interest, cd_interst_is_digit = check_user_input_float(input("What is the interest rate for the cd account? "))
+
+    while not cd_maturity_is_digit:
+        cd_maturity, cd_maturity_is_digit = check_user_input_int(input("What is the maturity(number of months) of the cd account? "))
 
     # Call the create_cd_account function and pass the variables from the user.
     updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    print(f"Updated cd balance: ${updated_cd_balance:,.2f}")
+    print(f"Interest earned: ${interest_earned:,.2f}")
 
 if __name__ == "__main__":
     # Call the main function.
+    main()
--- savings_account.py
+++ savings_account.py
@@ -1,5 +1,6 @@
 """Imports the SavingsAccount class and attributes from the Account.py file."""
 # ADD YOUR CODE HERE
+import Account
 
 # Define a function for the Savings Account
 def create_savings_account(balance, interest_rate, months):
@@ -17,18 +18,23 @@ def create_savings_account(balance, interest_rate, months):
     # Create an instance of the `Account` class and pass in the balance and interest parameters.
     #  Hint: You need to add the interest as a value, i.e, 0.
     # ADD YOUR CODE HERE
+    savings_account = Account.Account(balance = balance, interest = 0)
 
     # Calculate interest earned
      # ADD YOUR CODE HERE
+    interest_earned = balance * ((interest_rate/100) * (months/12))
 
     # Update the savings account balance by adding the interest earned
     # ADD YOUR CODE HERE
+    nova_balance = savings_account.balance + interest_earned
 
     # Pass the updated_balance to the set balance method using the instance of the SavingsAccount class.
     # ADD YOUR CODE HERE
+    savings_account.set_balance(nova_balance)
 
     # Pass the interest_earned to the set interest method using the instance of the SavingsAccount class.
     # ADD YOUR CODE HERE
+    savings_account.set_interest(interest_earned)
 
     # Return the updated balance and interest earned.
-    return  # ADD YOUR CODE HERE
+    return  nova_balance, interest_earned # ADD YOUR CODE HERE
